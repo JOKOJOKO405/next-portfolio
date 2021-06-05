2a5b9e0f81b222829ea1ef0e0a081d2800c14841
JOKOJOKO405 <biz.kj405@gmail.com>
2021-06-05 15:58:52 +0900

    add マークダウンファイルの読み込み

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,43 @@
-const Blog = () => {
+import matter from 'gray-matter'
+import Link from 'next/link'
+
+const Blog = (props) => {
   return (
     <>
-      <h1>ぶろぐのぺーじ</h1>
+      <h1>ブログのぺーじ</h1>
+      {props.blogs.map((blog, index) => 
+        <div key={index}>
+          <h2>{blog.frontmatter.title}</h2>
+          <p>{blog.frontmatter.data}</p>
+          <Link href={`/blog/${blog.slug}`}>read more</Link>
+        </div>
+      )}
     </>
   )
 }
 
 export default Blog
+
+export async function getStaticProps(){
+  const blogs = ((context) => {
+    const keys = context.keys() // マークダウンのデータ
+    const values = keys.map(context) // マークダウンの中身
+    
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
+      const val = values[index]
+      const document = matter(val.default)
+      return {
+        frontmatter: document.data,
+        slug: slug
+      }
+    })
+    return data
+  })(require.context('../data', true, /\.md$/)) // ファイルを読み込む
+  console.log(blogs)
+  return {
+    props: {
+      blogs: blogs
+    },
+  }
+}
